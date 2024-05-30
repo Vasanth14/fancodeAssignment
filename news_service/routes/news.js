e3fe79fecf3fc4a472ac7c216f4b5309f5b457e2
Vasanth Chandrasekar <35191203+Vasanth14@users.noreply.github.com>
2024-05-30 22:32:54 +0530

    news service

--- news_service/routes/news.js
+++ news_service/routes/news.js
@@ -2,12 +2,63 @@ const express = require('express');
 const router = express.Router();
 const News = require('../controllers/news')
 
-router.post('/news/match', News.createNewsForMatch);
-router.post('/news/tour', News.createNewsForTour);
-
-router.get('/news/match/:matchId', News.getNewsByMatchId);
-router.get('/news/tour/:tourId', News.getNewsByTourId);
-router.get('/news/sport/:sportId', News.getNewsBySportId);
+router.post('/news/match', async (req, res) => {
+    try {
+      await News.createNewsForMatch(req, res);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  });
+  
+  router.post('/news/tour', async (req, res) => {
+    try {
+      await News.createNewsForTour(req, res);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  });
+  
+  router.get('/news/match/:matchId', async (req, res) => {
+    try {
+      await News.getNewsByMatchId(req, res);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  });
+  
+  router.get('/news/tour/:tourId', async (req, res) => {
+    try {
+      await News.getNewsByTourId(req, res);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  });
+  
+  router.get('/news/sport/:sportId', async (req, res) => {
+    try {
+      await News.getNewsBySportId(req, res);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  });
 
 
 module.exports = router;
+
+
+//sample json object to create news using matchId
+// {
+//   "title": "test title",
+//   "description": "test desc",
+//   "tourId": 1,
+//   "matchId": 1
+// }
+
+
+//sample json object to create news using tourId
+// {
+//   "title": "test title",
+//   "description": "test desc",
+//   "tourId": 1,
+//   "sportId": 1
+// }
